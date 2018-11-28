357c5a97c8ae6ff8f1e821acf8e66e0a1e2582c9
congzw <2981810300@qq.com>
2018-11-28 12:46:53 +0800

    add missing file

---
+++ src/SimpleLogs/LogHelperExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SimpleLogs.Simple;
+
+namespace SimpleLogs
+{
+    public static class LogHelperExtensions
+    {
+        private static Func<ILoggerManager> _factory = () => new TraceLoggerManager();
+        public static Func<ILoggerManager> GetLoggerManagerFactory(this ILogHelper helper)
+        {
+            return _factory;
+        }
+
+        public static void SetLoggerManagerFactory(this ILogHelper helper, Func<ILoggerManager> factory)
+        {
+            if (factory == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _factory = factory;
+        }
+        
+        public static void ResetLoggerManagerFactory(this ILogHelper helper)
+        {
+            _factory = () => new TraceLoggerManager();
+        }
+
+        public static ILoggerManager GetLoggerManager(this ILogHelper helper)
+        {
+            return _factory();
+        }
+
+        public static ILogger GetLogger(this ILogHelper helper, Type type)
+        {
+            var loggerManager = helper.GetLoggerManager();
+            return loggerManager.GetLogger(type);
+        }
+
+        public static ILogger GetLogger(this ILogHelper helper, string type)
+        {
+            var loggerManager = helper.GetLoggerManager();
+            return loggerManager.GetLogger(type);
+        }
+    }
+}
